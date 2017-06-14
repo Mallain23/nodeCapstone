539367b460017fa3b0290737ba8e1ec17139a612
Mallain23 <mallain23>
2017-06-13 21:01:45 -0400

    added put and delete route

--- routes/resourceRouter.js
+++ routes/resourceRouter.js
@@ -57,4 +57,36 @@ router.post('/', (req, res) => {
     })
 
 
+router.put('/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`
+
+        console.error(message)
+        res.status(400).json({message: message})
+    }
+
+    const toUpdate {};
+    const updateableFields = ['title', 'content', 'course', 'type', 'professor'];
+
+    updateableFields.forEach(fields => {
+        if (field in req.body) {
+            toUpdate[field] = req.body[field]
+        }
+    })
+    StudyResources
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(resource => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal Server Error'}))
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    StudyResources
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal Server Error'}))
+})
+
 module.exports = router;
