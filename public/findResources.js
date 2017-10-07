52de952ac3c47c5979740e1540c371416645b63b
Mallain23 <mallain23>
2017-10-07 18:54:08 -0400

    focus fix

--- public/findResources.js
+++ public/findResources.js
@@ -271,21 +271,22 @@ const handleScroll = () => {
           if (target.length) {
         // Only prevent default if animation is actually gonna happen
               event.preventDefault();
-              $('html, body').animate({scrollTop: target.offset().top}, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-                  let $target = $(target);
-                  $target.focus();
-
-                  if ($target.is(":focus")) { // Checking if the target was focused
-                      return false;
-                  }
-
-                  else {
-                      $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                      $target.focus(); // Set focus again
-                  };
-              });
+              $('html, body').animate({scrollTop: target.offset().top}, 1000)
+          //     function() {
+          // // Callback after animation
+          // // Must change focus!
+          //         let $target = $(target);
+          //         $target.focus();
+          //
+          //         if ($target.is(":focus")) { // Checking if the target was focused
+          //             return false;
+          //         }
+          //
+          //         else {
+          //             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          //             $target.focus(); // Set focus again
+          //         };
+          //     });
 
           }
       }
